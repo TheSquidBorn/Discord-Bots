00bd3e0f4d065f7cffe533583ada60d7c7a26dae
TheSquidBorn <erik.winnerstam@gmail.com>
2018-10-28 17:21:56 +0100

    Add files via upload

--- S.A.M.RPI/tasks.py
+++ S.A.M.RPI/tasks.py
@@ -10,11 +10,11 @@ async def apod(client):
 	await client.wait_until_ready()
 	channel = discord.Object(id='485891401691955211')
 	while not client.is_closed:
-		seconds = getSeconds(16, 11)
+		seconds = getSeconds(8,00)
 		print("sleeping for " + str(seconds) + " seconds")
 		await asyncio.sleep(seconds)
 		channel = discord.Object(id='485891401691955211')
-		r = requests.get("https://api.nasa.gov/planetary/apod?api_key=JxHACldWATN21OaEW2MGZfpuzIRYMJIeLqZd1SWV&date=2018-09-07").json()
+		r = requests.get("https://api.nasa.gov/planetary/apod?api_key=JxHACldWATN21OaEW2MGZfpuzIRYMJIeLqZd1SWV").json()
 		title = "**" + r["title"] + "**\n"
 		text = r["explanation"]
 		url = r["url"]
@@ -31,10 +31,9 @@ async def apod(client):
 		else:
 			await client.send_message(channel, r["url"])
 		await client.send_message(channel, (title + text))
-		input()
 
 def getSeconds(hour, minute):
-	time = datetime.now() #+ timedelta(days=1)
+	time = datetime.now() + timedelta(days=1)
 	time = time.replace(hour=hour,minute=minute,second=0,microsecond=0)
 	delta = time - datetime.today()
 	seconds = int(delta.total_seconds())
